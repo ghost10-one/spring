9ae01794b7cbd1f4642b5f45c8e50fab5c1eb03c
Ghost10-one <azizsokhnamboup1o@gmail.com>
2024-11-02 23:08:38 -0100

    repository created

--- src/main/java/com/example/CRUD_SPRING/repository/CustomerRepository.java
+++ src/main/java/com/example/CRUD_SPRING/repository/CustomerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public abstract class CustomerRepository extends  {
+public interface CustomerRepository  extends JpaRepository<Customer , Long> {
 }
