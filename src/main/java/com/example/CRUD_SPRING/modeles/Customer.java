b88e0fd95042ee37e7e14071b8794778e69f7969
Ghost10-one <azizsokhnamboup1o@gmail.com>
2024-11-02 23:03:12 -0100

    first commit

---
+++ src/main/java/com/example/CRUD_SPRING/modeles/Customer.java
@@ -0,0 +1,49 @@
+package com.example.CRUD_SPRING.modeles;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private String name;
+    private  String email ;
+    private  String phone ;
+}
---
+++ src/main/java/com/example/CRUD_SPRING/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.CRUD_SPRING.repository;
+
+import com.example.CRUD_SPRING.modeles.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public abstract class CustomerRepository extends  {
+}
